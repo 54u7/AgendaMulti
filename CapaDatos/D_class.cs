98150ee8f708cd602e91464877645735f8478570
54u7 <99465123+54u7@users.noreply.github.com>
2022-07-25 14:57:46 -0400

    Add files via upload

---
+++ CapaDatos/D_class.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+using CapaEntidad;
+
+namespace CapaDatos
+{
+    public class D_class
+    {
+        SqlConnection conexion = new SqlConnection(ConfigurationManager.ConnectionStrings["conectar"].ConnectionString);
+    
+        public List<E_class> ListarDatos(string buscar)
+        {
+            SqlDataReader leerFilas;
+            SqlCommand cmd = new SqlCommand("sp_buscardatos", conexion);
+            cmd.CommandType = CommandType.StoredProcedure;
+            conexion.Open();
+
+            cmd.Parameters.AddWithValue("@buscar", buscar);
+
+            leerFilas = cmd.ExecuteReader();
+            
+            List<E_class> Listar = new List<E_class>();
+            while (leerFilas.Read())
+            {
+                Listar.Add(new E_class
+                {
+                    Id = leerFilas.GetInt32(0),
+                    Nombre = leerFilas.GetString(1),
+                    Apellidos = leerFilas.GetString(2),
+                    Telefono = leerFilas.GetString(3),
+                    Ocupacion = leerFilas.GetString(4)
+                });
+            }
+            conexion.Close();
+            leerFilas.Close();
+            return Listar;
+        }
+
+        public void InsertarDatos(E_class dato)//categoria
+        {
+            SqlCommand cmd = new SqlCommand("sp_insertardatos", conexion);
+            cmd.CommandType = CommandType.StoredProcedure;
+            conexion.Open();
+
+            cmd.Parameters.AddWithValue("@nombre", dato.Nombre);
+            cmd.Parameters.AddWithValue("@apellidos", dato.Apellidos);
+            cmd.Parameters.AddWithValue("@telefono", dato.Telefono);
+            cmd.Parameters.AddWithValue("@ocupacion", dato.Ocupacion);
+
+            cmd.ExecuteNonQuery();
+            conexion.Close();
+        }
+
+        public void EditarDatos(E_class dato)//categoria
+        {
+            SqlCommand cmd = new SqlCommand("sp_editardatos", conexion);
+            cmd.CommandType = CommandType.StoredProcedure;
+            conexion.Open();
+
+            cmd.Parameters.AddWithValue("@ID", dato.Id);
+            cmd.Parameters.AddWithValue("@nombre", dato.Nombre);
+            cmd.Parameters.AddWithValue("@apellidos", dato.Apellidos);
+            cmd.Parameters.AddWithValue("@telefono", dato.Telefono);
+            cmd.Parameters.AddWithValue("@ocupacion", dato.Ocupacion);
+
+            cmd.ExecuteNonQuery();
+            conexion.Close();
+        }
+
+        public void EliminarDatos(E_class dato)//categoria
+        {
+            SqlCommand cmd = new SqlCommand("sp_eliminardatos", conexion);
+            cmd.CommandType = CommandType.StoredProcedure;
+            conexion.Open();
+
+            cmd.Parameters.AddWithValue("@ID", dato.Id);
+
+            cmd.ExecuteNonQuery();
+            conexion.Close();
+        }
+    }
+}
+
+
+     
---
+++ CapaEntidad/E_class.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class E_class
+    {
+        private int _Id;
+        private string _Nombre;
+        private string _Apellidos;
+        private string _Telefono;
+        private string _Ocupacion;
+
+        public int Id { get => _Id; set => _Id = value; }
+        public string Nombre { get => _Nombre; set => _Nombre = value; }
+        public string Apellidos { get => _Apellidos; set => _Apellidos = value; }
+        public string Telefono { get => _Telefono; set => _Telefono = value; }
+        public string Ocupacion { get => _Ocupacion; set => _Ocupacion = value; }
+    }
+}
